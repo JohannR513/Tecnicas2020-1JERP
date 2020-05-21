c685b5264c15d05a4f3d25a2c11efd10d4916335
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2020-05-21 17:00:00 -0500

    Update FuncionesCC.h

--- Proyecto/FuncionesCC.h
+++ Proyecto/FuncionesCC.h
@@ -19,8 +19,15 @@ typedef struct Local{
 
 
 /*Agregar las funciones que necesite para satisfacer los requerimientos */
-void mostrarLocal(local_t ** centroComercial, int numPiso,
-		int numLocalxPiso, int numPisos, int numLocalesxPiso);
-
-
+//void mostrarLocal(local_t ** centroComercial, int numPiso,
+	//	int numLocalxPiso, int numPisos, int numLocalesxPiso);
+local_t** crearCC ( int numPiso, int numLocal );
+void ingresarLocal( local_t ** matrizMall, int numPiso, int numLocal );
+void listarLocales( local_t ** matrizMall, int numPiso, int numLocal );
+local_t* buscarPorNombre(local_t **matrizMall, int numPiso, int numLocal);
+local_t* buscarPorId( local_t ** matrizMall, int numPiso, int numLocal );
+void cambiarNombreLocal( local_t ** matrizMall, int numPiso, int numLocal );
+void eliminarLocal( local_t ** matrizMall, int numPiso, int numLocal );
+void localesOcupados( local_t ** matrizMall, int numPiso, int numLocal );
+void localesLibres( local_t ** matrizMall, int numPiso, int numLocal );
 #endif /* FUNCIONESCC_H_ */
